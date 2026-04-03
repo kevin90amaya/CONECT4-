f11d9bc85cda755d0f3f0d5965c0fa3dba34801d
Kevin90amaya <kevin90amaya@gmail.com>
2026-04-03 16:06:00 -0300

    controller y player view js

--- conect4/src/main/resources/static/js/players/controllers/ControllerPlayers.js
+++ conect4/src/main/resources/static/js/players/controllers/ControllerPlayers.js
@@ -1,5 +1,32 @@
 
+import PlayerView from "../views/PlayerView.js";
+import { ENDPOINTS } from "../../api/endpoints.js";
+
 class ControllerPlayers {
-    
+    playerView;
+
+    constructor() {
+        this.playerView = new PlayerView();
+    }
+
+    async getGameModes() {
+        const modes = await fetch(ENDPOINTS.GAME_MODES);
+        const data = await modes.json();
+        this.playerView.setGameMode(data);
+    }
+
+    async getPlayers() {
+        const players = await fetch(ENDPOINTS.PLAYERS);
+        const data = await players.json();
+        this.playerView.setPlayer(data);
+    }
+
+    async initialize() {
+        await this.getGameModes();
+        await this.getPlayers();
+        this.playerView.initialize();
+    }
+
+
 }
 export default ControllerPlayers;
---
+++ conect4/src/main/resources/static/js/players/views/PlayerView.js
@@ -0,0 +1,19 @@
+
+class PlayerView {
+    gameMode;
+    players;
+
+    setPlayer(players){
+        this.players = players;
+    }
+
+    setGameMode(modes){
+        this.gameMode = modes;
+    }
+
+    initialize(){
+        // TODO: Initialize the view
+    }
+
+}
+export default PlayerView;
