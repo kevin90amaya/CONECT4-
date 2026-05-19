df27a4d8cb401e76fbfe06953f4732c9043eabfd
Kevin90amaya <kevin90amaya@gmail.com>
2026-05-19 11:22:34 -0300

    Add ZzFX-based sound effects
    Import zzfx and implement SoundEngine playback methods. playHover, playSelect, playDrop, playError, playWin (with a second tone delayed), and playDraw now call zzfx to produce sounds and honor isMuted; playHover also checks audioUnlocked. This enables actual audio feedback for UI events.

--- conect4/src/main/resources/static/js/utils/sound/SoundEngine.js
+++ conect4/src/main/resources/static/js/utils/sound/SoundEngine.js
@@ -1,5 +1,7 @@
 // ZzFX - Zuper Zmall Zound Zynth - Micro Edition
 // MIT License - Copyright 2019 Frank Force
+import { zzfx } from './ZzFX.js';
+
 class SoundEngine {
     constructor() {
         this.isMuted = false;
@@ -19,12 +21,36 @@ class SoundEngine {
         this.audioUnlocked = true;
     }
 
-    playHover() {}
-    playSelect() {}
-    playDrop() {}
-    playError() {}
-    playWin() {}
-    playDraw() {}
+    playHover() { 
+        if (this.isMuted || this.audioUnlocked === false) return;
+        zzfx(1, 0.05, 150, 0, 0, 0.05, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0); 
+    }
+
+    playSelect() { 
+        if (this.isMuted) return;
+        zzfx(1, 0.05, 400, 0.01, 0.01, 0.1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0); 
+    }
+
+    playDrop() { 
+        if (this.isMuted) return;
+        zzfx(1, 0.05, 300, 0.01, 0.05, 0.1, 0, 1, 0, 0, -200, 0, 0, 0, 0, 0, 0, 0.5, 0, 0); 
+    }
+
+    playError() { 
+        if (this.isMuted) return;
+        zzfx(1, 0.1, 100, 0.05, 0.1, 0.2, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.7, 0, 0); 
+    }
+
+    playWin() { 
+        if(this.isMuted) return;
+        zzfx(1, 0.1, 400, 0.05, 0.2, 0.5, 0, 1, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0.5, 0, 0); 
+        setTimeout(() => zzfx(1, 0.1, 600, 0.05, 0.2, 0.8, 0, 1, 0, 0, 200, 0, 0, 0, 0, 0, 0, 0.6, 0, 0), 150); 
+    }
+
+    playDraw() { 
+        if (this.isMuted) return;
+        zzfx(1, 0.1, 200, 0.1, 0.4, 0.5, 0, 1, 0, 0, -100, 0, 0, 0, 0, 0, 0, 0.5, 0, 0); 
+    }
 }
 
 export default SoundEngine;
